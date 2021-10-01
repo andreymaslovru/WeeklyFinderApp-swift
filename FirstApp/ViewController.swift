ab696cddd3c84bfb7d6d7edab6aff471232f3edf
Andrey Maslov <andrey.ma2001@gmail.com>
2021-10-01 15:44:04 +0700

    finished first pro

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dayTF: UITextField!
+    
+    @IBOutlet weak var monthTF: UITextField!
+    
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func onPress(_ sender: Any) {
+        guard let day = dayTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        guard let date = calendar.date(from: dateComponents) else { return }
+        let weekday = dateFormatter.string(from: date)
+        let capitalized = weekday.capitalized
+        result.text = capitalized
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
